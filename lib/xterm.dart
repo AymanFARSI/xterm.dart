017830540ef75e7c8b8127918b395aaf5ec44853
SincerelyFaust <hedesmarin@gmail.com>
2022-08-19 18:56:07 +0200

    chore: delete everything

--- lib/xterm.dart
+++
@@ -1,7 +0,0 @@
-library xterm;
-
-export 'terminal/terminal.dart';
-export 'terminal/terminal_backend.dart';
-export 'terminal/terminal_ui_interaction.dart';
-export 'terminal/platform.dart';
-export 'theme/terminal_style.dart';
